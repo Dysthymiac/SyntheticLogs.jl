64a3be80cffab72891b8766e0d78822e980c6b89
Documenter.jl <documenter@juliadocs.github.io>
2024-08-13 12:24:08 +0000

    build based on 51e355f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SyntheticLogs","category":"page"},{"location":"#SyntheticLogs","page":"Home","title":"SyntheticLogs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SyntheticLogs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SyntheticLogs]","category":"page"}]
+}
